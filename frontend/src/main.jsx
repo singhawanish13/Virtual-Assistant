2adf82e2772b81845d7aef5cc86f39defa218c82
Divya Singh <singhawanish138>
2025-09-27 18:38:53 +0530

    first commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
-import UserContext from './context/userContext.jsx'
+import UserContext from './context/UserContext.jsx'
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
-<UserContext>
+  <UserContext>
     <App />
   </UserContext>
-  </BrowserRouter>
+</BrowserRouter>
  
 )
